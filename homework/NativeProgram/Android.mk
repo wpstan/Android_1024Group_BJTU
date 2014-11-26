0fbc2350c385e72471b502d4638377ad54723a29
tanshuai <14126140@bjtu.edu.cn>
2014-11-27 00:53:47 +0800

    1024Group_NativeProgram_Homework

---
+++ homework/NativeProgram/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= hello1024.c
+LOCAL_MODULE := tanshuai
+include $(BUILD_EXECUTABLE)
+
---
+++ homework/NativeProgram/hello1024.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  printf("********************\n");
+  printf("*We are group 1024!*\n");
+  printf("********************\n");
+  return 0;
+}
